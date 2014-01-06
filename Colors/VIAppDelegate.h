ed05a856ed88eed9fc0bcfa294338f664040339f
黄显宏 <to_top@126.com>
2014-01-06 20:56:08 +0800

    Initial Commit

---
+++ Colors/VIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VIAppDelegate.h
+//  Colors
+//
+//  Created by mk on 14-1-6.
+//  Copyright (c) 2014年 Vnidev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VIAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Colors/VIAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  VIAppDelegate.m
+//  Colors
+//
+//  Created by mk on 14-1-6.
+//  Copyright (c) 2014年 Vnidev. All rights reserved.
+//
+
+#import "VIAppDelegate.h"
+
+@implementation VIAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
